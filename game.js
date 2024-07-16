58ba78cd42be3ea3d2d945c0483f71e2a35e6b09
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-16 18:21:58 +0500

    Add playRound() for one round of game

--- game.js
+++ game.js
@@ -6,22 +6,70 @@ function getComputerChoice()
     else if(result===1)
         return "paper";
     else if(result===2)
-        return "scissors";
+        return "scissor";
     else
         return "error";
 }
 function getHumanChoice()
 {
     let humanChoice=prompt("rock , paper or scissor ?");   
+    humanChoice=humanChoice.toLowerCase();
     if(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissor" )
     {
         return alert("Check spelling and make sure all are small letters");
     }
     return humanChoice;
 }
-let choice=getComputerChoice();
-console.log(choice);
+function playRound(humanSelection,computerSelection)
+{
+    if(humanSelection===computerSelection)
+        return alert("Draw");
+    else if(humanSelection==="rock")
+    {
+        if(computerSelection==="paper")
+        {
+            computerScore++;
+            return alert(`You Lose ${computerSelection} beats ${humanSelection}`);
+        }
+        else if(computerSelection==="scissor")
+        {
+            humanScore++;
+            return alert(`You Win ${humanSelection} beats ${computerSelection}`);
+            
+        }
+    }
+    else if(humanSelection==="paper")
+    {
+        if(computerSelection==="scissor")
+        {
+            computerScore++;
+            return alert(`You Lose ${computerSelection} beats ${humanSelection}`);
+        }
+        else if(computerSelection==="rock")
+        {
+            humanScore++;
+            return alert(`You Win ${humanSelection} beats ${computerSelection}`);
+        }
+    }
+    else if(humanSelection==="scissor")
+        {
+            if(computerSelection==="rock")
+            {    computerScore++;
+                return alert(`You Lose ${computerSelection} beats ${humanSelection}`);
+            }
+            else if(computerSelection==="paper")
+            {
+                humanScore++;
+                return alert(`You Win ${humanSelection} beats ${computerSelection}`);
+            }
+        }       
+}
 
-choice=getHumanChoice();
-console.log(choice);
-
+const humanSelection=getComputerChoice();
+const computerSelection=getHumanChoice();
+console.log(humanSelection);
+console.log(computerSelection);
+let humanScore=0,computerScore=0;
+playRound(humanSelection,computerSelection);
+console.log(humanScore);
+console.log(computerScore);
